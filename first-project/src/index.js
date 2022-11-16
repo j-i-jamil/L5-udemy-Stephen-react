a0132c18fbc42ba4692721c84ffb9b7639dac1b5
Jamil <jamiulislamjamil@gmail.com>
2022-11-16 18:27:35 +0600

    November 16, 20222

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -1,17 +1,10 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a react component
+// const App = () => <div>Hi there</div>;
+const App = () => {
+	return <div>Hi, This is Jamil</div>;
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
