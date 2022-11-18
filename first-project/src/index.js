a5d4ad814cabfc0fae9e743c2c39b7a2dfd9a05a
Jamil <jamiulislamjamil@gmail.com>
2022-11-18 23:14:39 +0600

    November 18, 2022

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -1,10 +1,28 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import { createRoot } from 'react-dom/client'
 // Create a react component
-// const App = () => <div>Hi there</div>;
+
 const App = () => {
-	return <div>Hi, This is Jamil</div>;
+	const buttonText = 'Click Me';
+	const buttonText2 = {
+		click: 'Click me',
+	};
+	const buttonText3 = ['Click', 'Me'];
+	return (
+		<div>
+			<label htmlFor="name" className="label">
+				Enter name: {}
+			</label>
+			<input type="text" id="name" />
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText2.click}</button>
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText3}</button>
+		</div>
+	);
 };
+
+// Render
 ReactDOM.render(<App />, document.querySelector('#root'));
+// createRoot(document.querySelector('.root')).render(<h1>Jamil</h1>)
